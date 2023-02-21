ae5d33bd50a51774c43571134bb841b9da1d9c23
Danny <dannymou7426@hotmail.com>
2023-02-20 21:50:10 -0600

    adjusted color

--- table.py
+++ table.py
@@ -4,13 +4,6 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication
 
 
-class ProdWindow(QDialog):
-    def __init__(self):
-        super(ProdWindow, self).__init__()
-        loadUi("prodtable.ui", self)
-
-    def loaddata(self):
-        med=[]
 
 #main
 app = QApplication(sys.argv)
